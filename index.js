0f5525d1abed01f23e6a3e3ea1a39380cd4716ab
Melissa <105521312+k0106@users.noreply.github.com>
2022-10-20 08:20:09 -0500

    Servidor
    Servidor listo.

--- index.js
+++ index.js
@@ -3,12 +3,22 @@
 const server = http.createServer((req,res) => {
     res.status =200;
     res.setHeader('Content-Type','text/plain');
-    res.end('Bienvenidos');/**llamar metodo */
-}); 
-/*
+    res.end('Bienvenidos');
+}); /** 
 server.listen(3000,()=> {
     console.log('Serve on port 3000')
     
-})
+}); */
 
-/*Express trae http asi que no es necesario escribir este codigo,solo se necesita para iniciar.
+
+
+/*Express trae http asi que no es necesario escribir este codigo,solo se necesita para iniciar.**/
+
+
+const express  = require('express'); /*este modulo es quien agrega http**/
+const app =express();
+
+app.listen(3000,() => {
+    console.log('Server on Port 3000');
+
+});
