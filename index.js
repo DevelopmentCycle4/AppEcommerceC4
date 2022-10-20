8a2f0ec1c00de366701f40fcb83764f191fd4edf
Melissa <105521312+k0106@users.noreply.github.com>
2022-10-20 08:28:03 -0500

    GET
    Creando la respuesta inicial del servidor.
    'Bienvenido a tu tienda online'-

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ server.listen(3000,()=> {
 const express  = require('express'); /*este modulo es quien agrega http**/
 const app =express();
 
+app.get ('/'(req,res) =>{
+    res.send('Bienvenido a tu tienda Online')
+}
+)
 app.listen(3000,() => {
     console.log('Server on Port 3000');
 
