fe561f44508a6a7812cb1762ecbb8bb0113f8dab
Melissa <105521312+k0106@users.noreply.github.com>
2022-10-27 23:05:07 -0500

    .
    cambios

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const cors=require('cors')
+
+const app=express()
+const port= 3000
+
+
+
+app.get('./prueba',(req,res)=>{
+    res.send('Servidor listo')
+})
+
+app.listen(port,()=>{
+    console.log('Estoy ejecutandome en http://localhost:${port}')
+})
+
+GET=>micomputadora/pokemones=>Devuelve los pokemones
+POST=>micomputadora/pokemones con {pokemon}=>guarda el pokemon
