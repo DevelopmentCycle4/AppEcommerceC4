3f26dfa1de10631bc5212c99481ae672c75815a5
econowil <111374997+econowil@users.noreply.github.com>
2022-11-09 00:08:38 -0500

    Update db.js
    conexión con mongodb

--- backend/server/db.js
+++ backend/server/db.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://mongodb+srv://EcommerceC4:<pUkiCEbQ0NpsY8Uq>@cluster0.ozricgr.mongodb.net/test", {})
-.then(() => console.log("Conectado a MongoDb"))
-.catch((err)=> console.log(err))
 
+export async function DBconnection(){
+    try{
+        const db = await mongoose.connect(
+            "mongodb+srv://EcommerceC4:pUkiCEbQ0NpsY8Uq@cluster0.0qlbckm.mongodb.net/EcommerceC4"
+        );
+        console.log("Estableciendo Coneccion a" , db.connection.name);
+    } catch(error) {
+        console.log(error.message);
+    }
+}
 const esquema = mongoose.Schema({
     nombre:String,
     precio:Number,
