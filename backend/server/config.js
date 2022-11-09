9308864565bf63a6e613919029980e3705bf2e05
econowil <111374997+econowil@users.noreply.github.com>
2022-11-09 01:55:04 -0500

    Update config.js

--- backend/server/config.js
+++ backend/server/config.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 dotenv.config({path: './.env'});
 
 
-export const MONGODB_URI = process.env.MONGODB_URI; 
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://cluster0.0qlbckm.mongodb.net/testdb"
 
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 5000
+
 
