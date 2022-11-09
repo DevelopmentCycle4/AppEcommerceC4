393bdd150bb083b2bb0b6b8b210b4eb14170748b
econowil <111374997+econowil@users.noreply.github.com>
2022-11-09 01:43:01 -0500

    Update index.js

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,11 +1,12 @@
 import express  from 'express';
 import productsRoutes from "./routes/productsroutes.js"
 import {DBconnection} from './db.js'
+import { PORT } from './config.js';
 //import {consultar} from './db.js'
 
 const app=express()
 DBconnection();
-const port= 4000
+//const port= 4000
 app.use(productsRoutes)
 
 
@@ -13,8 +14,9 @@ app.get('./prueba',(req,res)=>{
     res.send('Servidor listo')
 })
 
-app.listen(4000);
-    console.log('Estoy ejecutandome en puerto', 4000);
+app.listen(PORT);
+    console.log('Estoy ejecutandome en puerto', PORT);
 
 
 //consultar()
+
